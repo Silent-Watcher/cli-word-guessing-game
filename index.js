439dfe056f9aec3631f1bfbcf5879c62da5a1e4e
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-05-25 14:56:19 +0430

    import modules

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+#!/usr/bin/env node
 'use strict';
-
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+import inquirer from 'inquirer';
+import { createSpinner } from 'nanospinner';
